046f78a806b3e764140ff747acd6f256ee7e5670
JordanDiaz01 <jordandelangel.d@gmail.com>
2023-02-10 08:48:53 -0600

    usando express

---
+++ Parcial1/Express/index.js
@@ -0,0 +1,27 @@
+
+const express= require('express')
+const app  = express()
+
+app.use(express.json())
+app.get('/alumnos/:carrera',(req,res)=>{
+    console.log(req.params.carrera)
+    res.json({alumnos:"Carrera de sistemmas de"+ req.params.carrera})
+})
+
+app.get('/maestros',(req,res)=>{
+    console.log(req.query.control)
+    res.json({Maestro:"Información del maestro "+ req.query.control})
+
+})
+app.get('/administrativos',(req,res)=>{
+    console.log(req.body.Nombre)
+    res.json({PersonalAdmin:"Información del personal admin  "+ req.body.Nombre})
+
+})
+
+app.use((req,res)=>{
+    res.status(404).json({status:'Página no encontrada'})
+})
+app.listen(8080,()=>{
+    console.log("Running...")
+})
