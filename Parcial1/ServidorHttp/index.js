e093891b934dc7848de4821848fb3c9f97e6044e
JordanDiaz01 <jordandelangel.d@gmail.com>
2023-02-09 20:50:50 -0600

    cors

--- Parcial1/ServidorHttp/index.js
+++ Parcial1/ServidorHttp/index.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 //create a server object:
 http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin','*')
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
   }).listen(8080,()=>console.log('servidor http listo')); //the server object listens on port 8080
