362fd978a76c145c87220c9455018e6e98c4a81c
JordanDiaz01 <jordandelangel.d@gmail.com>
2023-03-09 08:43:09 -0600

    jsdoc

---
+++ Parcial2/jsdoc/index.js
@@ -0,0 +1,3 @@
+import * as mat from "./mate.js";
+
+console.log(mat.AreaCuadrado(2))
---
+++ Parcial2/jsdoc/mate.js
@@ -0,0 +1,21 @@
+/**
+ * Esta función regresa el area de un cuadrado
+ * @param {Number} lado medida del lado del cuadrado 
+ * @returns Number area del cuadrado
+ */
+export const AreaCuadrado = (lado)=> lado*lado 
+
+/**
+ * 
+ * @param {Number} base medida de la base del triangulo 
+ * @param {Number} altura medida de la altura del triangulo 
+ * @returns Number Area del triangulo
+ */
+export const AreaTriangulo = (base,altura) => base*altura/2
+
+/**
+ * 
+ * @param Number} radio medida del radio del circulo 
+ * @returns Number devuelve el calculo del area del circulo
+ */
+export const AreaCirculo = (radio) => Math.PI * radio **2
