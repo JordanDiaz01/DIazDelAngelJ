d9af8b933f99ed7f6a066bfd12dce8639548eb27
JordanDiaz01 <jordandelangel.d@gmail.com>
2023-04-28 08:11:45 -0500

    jest testing

---
+++ Parcial3/JestTesting/modulo.js
@@ -0,0 +1,8 @@
+
+
+const areaTriangulo = (a,b) => b*a/2
+
+
+
+
+module.exports = areaTriangulo
---
+++ Parcial3/JestTesting/modulo.test.js
@@ -0,0 +1,6 @@
+const areaTriangulo  = require('./modulo.js')
+
+
+test('test',()=>{
+    expect(areaTriangulo(5,2)).toBe(5)
+})
