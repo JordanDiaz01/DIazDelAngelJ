06ac84dc4819cc9580109903f08c031d900e2406
jordandiaz01 <jordandelangel.d@gmail.com>
2023-05-11 23:21:39 -0500

    swagger

--- Parcial3/Swagger/index.js
+++ Parcial3/Swagger/index.js
@@ -6,21 +6,56 @@ const cors = require('cors')
 const app = express()
 const swaggerUI = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
+const { SwaggerTheme } = require('swagger-themes');
+const redoc = require('redoc-express');
+//Thema swagger json
+const theme = new SwaggerTheme('v3');
 
+const options = {
+    explorer: true,
+    customCss: theme.getBuffer('dark')
+  };
 
+const ContenidoReadme = fs.readFileSync(path.join(__dirname)+'/README.md',{encoding:'utf8',flag:'r'})
+const apidef_string = fs.readFileSync(path.join(__dirname)+'/APIdef.json',{encoding:'utf8',flag:'r'})
+const apidef_objeto = JSON.parse(apidef_string)
+apidef_objeto.info.description=ContenidoReadme;
 const swaggerOptions = {
-    definition: {
-    openapi: '3.0.0',
-    info: {
-    title: 'API Empleados',
-    version: '1.0.0',
-    },
-    servers:[
-    {url: "http://localhost:8082"}
-    ],
-    },
+    definition: apidef_objeto,
     apis: [`${path.join(__dirname,"./index.js")}`],
-    };
+};
+
+let redocTheme_objeto = JSON.parse(apidef_string)
+/**
+ * @swagger
+ * 
+ * components:
+ *   schemas:
+ *     empleado: 
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Identificador del empleado    
+ *           example: "jordan"
+ */
+
+
+
+
+// const swaggerOptions = {
+//     definition: {
+//     openapi: '3.0.0',
+//     info: {
+//     title: 'API Empleados',
+//     version: '1.0.0',
+//     },
+//     servers:[
+//     {url: "http://localhost:8082"}
+//     ],
+//     },
+//     apis: [`${path.join(__dirname,"./index.js")}`],
+//     };
 
 
 app.use(express.json())
@@ -28,15 +63,18 @@ app.use(express.text())
 app.use(cors())
 
 
-/**
-* @swagger
-* /empleado:
-*   get:
-*       description: ruta de empleados!
-*       responses:
-*         200:
-*            description: Returns a mysterious string.
-*/
+  /**
+ * @swagger
+ * /usuario:
+ *  get:
+ *    tags:
+ *      - empleado
+ *    summary: Consultar todos los empleados
+ *    description: Petición Get a la ruta de empleados
+ *    responses:
+ *      200:
+ *        description: Devuelve todos los empleados de la base de datos en un json
+ */
 app.get('/empleados/',async(req,res)=>{
 
      const connection = await mysql.createConnection({host:'localhost', user: 'root', password: '1234',database: 'proyectoweb'});
@@ -45,6 +83,29 @@ app.get('/empleados/',async(req,res)=>{
     
     res.json(rows);
 })
+
+/**
+ * @swagger
+ * /empleados/:nombre:
+ *  get:
+ *    tags:
+ *      - empleado
+ *    summary: Busca un empleado por nombre
+ *    description: Petición Get
+ *    parameters:
+ *      - name: nombre
+ *        in: path
+ *        description: nombre del usuario a consultar
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *    responses:
+ *      200:
+ *        description: Regresa el empleado solicitado
+ *      400:
+ *        description: No se encontro el empleado
+ */
 app.get('/empleados/:nombre',async(req,res)=>{
     
      const connection = await mysql.createConnection({host:'localhost', user: 'root', password: '1234',database: 'proyectoweb'});
@@ -58,6 +119,24 @@ app.get('/empleados/:nombre',async(req,res)=>{
     res.json(rows);
 })
 
+ /**
+ * @swagger
+ * /id:
+ *   post:
+ *     tags:
+ *       - empleado
+ *     summary: Registrar un empleado
+ *     description: Petición Post a la ruta de empleados para ingresar un nuevo registro
+ *     requestBody:
+ *       description: Crea un nuevo empleado
+ *       content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/empleado'
+ *     responses:
+ *       200:
+ *         description: Empleado insertado con éxito
+ */
 app.post('/empleados',async(req,res)=>{
    
     const connection = await mysql.createConnection({host:'localhost', user: 'root', password: '1234',database: 'proyectoweb'});
@@ -69,6 +148,25 @@ app.post('/empleados',async(req,res)=>{
     //await connection.query('insert into empleados SET ? ',{nombre,ap_paterno,ap_materno,edad,domicilio,ciudad,estado,codigo_postal,correo,curp,rfc})
 })
 
+/**
+ * @swagger
+ * /empleados/:nombre:
+ *  patch:
+ *    tags:
+ *      - empleado
+ *    summary: Actualizar empleado
+ *    description: Peticion de tipo put a la ruta de empleados
+ *    requestBody:
+ *       description: Actualiza los datos de un empleado
+ *       content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/empleado'
+ *       required: true
+ *    responses:
+ *      200:
+ *        description: Empleado actualizado con éxito
+ */
 app.put('/empleados/:nombre',async(req,res)=>{
     const nombre= req.params.nombre
     const connection = await mysql.createConnection({host:'localhost', user: 'root', password: '1234',database: 'proyectoweb'});
@@ -77,6 +175,28 @@ app.put('/empleados/:nombre',async(req,res)=>{
 
     rows.affectedRows==1 ?  res.status(200).send('Empleado actualizado con éxito!') : res.status(500).send('Ocurrió un error en el servidor')
 })
+
+/**
+ * @swagger
+ * /empleados/:nombre:
+ *   delete:
+ *     tags:
+ *       - empleado
+ *     summary: Se borra un empleado
+ *     description: Petición Delete a la ruta de Empleados para borrar un empleado por medio de un nombre.
+ *     parameters:
+ *       - name: nombre
+ *         in: path
+ *         description: nombre del empleado a ELIMINAR
+ *         required: true
+ *         schema:
+ *          type: string
+ *          format: string
+ *     responses:
+ *       200:
+ *         description: Empleado eliminado con éxito
+ */
+
 app.delete('/empleados/:nombre',async(req,res)=>{
     const nombre= req.params.nombre
     const connection = await mysql.createConnection({host:'localhost', user: 'root', password: '1234',database: 'proyectoweb'});
@@ -84,13 +204,34 @@ app.delete('/empleados/:nombre',async(req,res)=>{
     rows.affectedRows==1 ?  res.status(200).send('Empleado eliminado con éxito!') : res.status(500).send('Ocurrió un error en el servidor')
 })
 
+
+//swagger
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs,options));
+
+app.get("/docs.json",(req,res)=>{
+    res.json(swaggerDocs);
+})
+
+app.get(
+    '/redocs',
+    redoc({
+      title: 'API Docs',
+      specUrl: '/docs.json',
+      nonce: '', // <= it is optional,we can omit this key and value
+      // we are now start supporting the redocOptions object
+      // you can omit the options object if you don't need it
+      // https://redocly.com/docs/api-reference-docs/configuration/functionality/
+      redocOptions: {
+        theme: redocTheme_objeto
+      }
+    })
+  );
 
 app.use((req,res)=>{
     res.status(404).json({estado:"Ruta no encontrada"})
 })
 
 app.listen(8082,()=>{
-    console.log("Running...")
+    console.log("Running in Port 8082")
 })
---
+++ Parcial3/Swagger/readme.md
@@ -0,0 +1,6 @@
+## EMPLEADOS 
+
+Este recurso hace un abc (CRUD) de un esquema de empleados.
+
+
+![Texto alternativo](https://www.tecnm.mx/assets/files/main/img/pleca_tecnm.jpg?img=26)
